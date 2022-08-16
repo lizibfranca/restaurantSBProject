7aaad6a5f6561dfe56c0d1a332d86183b66ec9d3
= <=>
2022-08-16 15:43:43 +0100

    new services

---
+++ demo/src/main/java/com/example/demo/service/PermissionService.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.permission.Permission;
+import com.example.demo.repository.PermissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PermissionService {
+    private final PermissionRepository permissionRepository;
+
+    public Page<Permission> listPage(Pageable pageable){
+        return permissionRepository.findAll(pageable);
+    }
+
+    public List<Permission> listAll(){
+        return permissionRepository.findAll();
+    }
+
+    public Permission getPermissionById(long id) {
+        return permissionRepository
+                .findById(id)
+                .orElseThrow(() -> new BadRequestException("Permission not found"));
+
+    }
+
+    @Transactional
+    public Permission save(Permission permission) {
+        return permissionRepository.save(permission);
+    }
+
+    @Transactional
+    public void update(Long id, Permission dto) {
+        permissionRepository.findById(id);
+        permissionRepository.save(dto);
+    }
+
+    public void delete(Long id) {
+        permissionRepository.findById(id);
+        permissionRepository.deleteById(id);
+    }
+}
